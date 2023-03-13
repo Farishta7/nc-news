190dd2aeb69b7241fc1a5611f403d53c228f8bbb
Farishta7 <farishtatq@gmail.com>
2023-03-13 15:26:11 +0000

    created components and incorporated routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TopicsList from "./components/TopicsList";
+import {Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/topics' element={<TopicsList />}/>
+      </Routes>
+    
     </div>
   );
 }
