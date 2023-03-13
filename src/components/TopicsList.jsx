62d5573727cb599f2143cbabd3c09cab18cc9ea2
Farishta7 <farishtatq@gmail.com>
2023-03-13 15:27:24 +0000

    Topics list for future use

---
+++ src/components/TopicsList.jsx
@@ -0,0 +1,12 @@
+const TopicsList = () => {
+    return (
+        <div>
+            <p>Please select a topic to read articles about:</p>
+            <button>Football</button>
+            <button>Coding</button>
+            <button>Cooking</button>
+        </div>
+        )
+  };
+  
+export default TopicsList;
