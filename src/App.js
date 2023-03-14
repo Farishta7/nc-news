3d01263d88bcb1490d9116b55fb4bf951f96f8e4
Farishta7 <farishtatq@gmail.com>
2023-03-14 11:02:03 +0000

    created isLoading on app to be able to use in other components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import TopicsList from "./components/TopicsList";
+import SingleArticle from './components/SingleArticle';
 import {Route, Routes} from 'react-router-dom';
-
+import { useState } from "react";
 
 function App() {
+
+    const [isLoading, setIsloading] = useState(true);
+
+    
+
   return (
     <div className="App">
       <Header />
       <Nav />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/topics' element={<TopicsList />}/>
+        <Route path='/' element={<Home isLoading={isLoading} setIsloading={setIsloading}/>}/>
+        <Route path='/articles/:article_id' element={<SingleArticle isLoading={isLoading} setIsloading={setIsloading} />}/>
       </Routes>
     
     </div>
