6b8f17d5a4b63e88e7d47fb14561abb515363f27
Farishta7 <farishtatq@gmail.com>
2023-03-16 13:36:47 +0000

    add components for each topic articles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import SingleArticle from './components/SingleArticle';
 import {Route, Routes} from 'react-router-dom';
+import TopicsList from './components/TopicsList';
+import AllFootballArticles from './components/AllFootballArticles';
+import AllCodingArticles from './components/AllCodingArticles';
+import AllCookingArticles from './components/AllCookingArticles';
 
 function App() {  
   return (
@@ -13,6 +17,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/articles/:article_id' element={<SingleArticle  />}/>
+        <Route path='/topics' element={<TopicsList />}/>
+        <Route path='/topics/football' element={<AllFootballArticles />}/>
+        <Route path='/topics/coding' element={<AllCodingArticles />}/>
+        <Route path='/topics/cooking' element={<AllCookingArticles />}/>
       </Routes>
     
     </div>
