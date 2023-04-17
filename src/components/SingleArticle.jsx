b1681d73642d22b7f7f22553f3424c44af6da6c7
Farishta7 <farishtatq@gmail.com>
2023-04-17 16:03:48 +0100

    adjusted header and voting section

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -58,29 +58,31 @@ const SingleArticle = () => {
     if (isLoading) return <p>Hang in there! Loading...</p>
 
     return (
-        <>
-            <h3 style={{margin: "3rem"}}>{singleArticle.title}</h3>
-            <section className="single-article-author-date">
+        <div style={{margin: "3rem"}}>
+        <div className="header-and-image">
+            <h3 className="single-article-header">{singleArticle.title}</h3>
             <img src={singleArticle.article_img_url} className="single-article-image" alt="I am an alt."/>
-            
-                <p>Author: {singleArticle.author}</p>
-                <p>Date posted: {new Date(singleArticle.created_at).toLocaleString().split(',')[0]}</p>
-            
-            <button onClick={() => upVote(singleArticle.article_id)}>
-                <span aria-label="up vote for this article">👍</span>
-            </button>
-
-            <p>{singleArticle.votes}</p>
-
-            <button onClick={() => downVote(singleArticle.article_id)}>
+             
+        </div>
+        <div style={{marginTop: "20px"}}>
+            <p className="author-and-date">Author: {singleArticle.author} &ensp; Date posted: {new Date(singleArticle.created_at).toLocaleString().split(',')[0]}</p>
+        </div>
+        <div>
+            <button className="voting-section" onClick={() => upVote(singleArticle.article_id)}>
+                    <span aria-label="up vote for this article">👍</span>
+                </button>
+                <p className="voting-section" >&ensp;{singleArticle.votes}&ensp;</p>
+                <button className="voting-section" onClick={() => downVote(singleArticle.article_id)}>
                 <span aria-label="down vote for this article">👎</span>
-            </button>
-            </section>
-            {isVotingError && <p>Your vote was NOT processed!</p>}
-            <p className="article-body">{singleArticle.body}</p>
+                </button>
+                {isVotingError && <p>Your vote was NOT processed!</p>}
+        </div>
+        <p className="article-body">{singleArticle.body}</p>
             <ArticleComments />
+        </div>
+           
             
-        </>
+        
         )
   };
   
