950382309cdac8a441db4598d79fca84c1027582
Farishta Tariq <farishtatq@gmail.com>
2023-10-06 17:41:00 +0100

    formatted using prettier

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,51 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
-const ncnewsApi = axios.create({baseURL: "https://farishtas-project.onrender.com/api"});
+const ncnewsApi = axios.create({
+  baseURL: "https://farishtas-project.onrender.com/api",
+});
 
 export const getArticles = () => {
-    return ncnewsApi
-    .get('/articles')
-    .then((response) => {
-        return response.data.articles;
-    })
-}
+  return ncnewsApi.get("/articles").then((response) => {
+    return response.data.articles;
+  });
+};
 
 export const getSingleArticle = (article_id) => {
-    return ncnewsApi
-    .get(`/articles/${article_id}`)
-    .then((response) => {
-        return response.data.article;
-    })
-}
+  return ncnewsApi.get(`/articles/${article_id}`).then((response) => {
+    return response.data.article;
+  });
+};
 
 export const getComments = (article_id) => {
-    return ncnewsApi
-    .get(`/articles/${article_id}/comments`)
-    .then((response) => {
-        return response.data.articleComments;
-    })
-}
+  return ncnewsApi.get(`/articles/${article_id}/comments`).then((response) => {
+    return response.data.articleComments;
+  });
+};
 
 export const ArticleUpVote = (article_id) => {
-    return ncnewsApi
-    .patch(`/articles/${article_id}`, {inc_votes: 1})
+  return ncnewsApi
+    .patch(`/articles/${article_id}`, { inc_votes: 1 })
     .then((response) => {
-        return response.data.votes;
-    })
-}
+      return response.data.votes;
+    });
+};
 
 export const ArticleDownVote = (article_id) => {
-    return ncnewsApi
-    .patch(`/articles/${article_id}`, {inc_votes: -1})
+  return ncnewsApi
+    .patch(`/articles/${article_id}`, { inc_votes: -1 })
     .then((response) => {
-        return response.data.votes;
-    })
-}
+      return response.data.votes;
+    });
+};
 
 export const postComment = (article_id, newComment) => {
-    return ncnewsApi
-    .post(`/articles/${article_id}/comments`, {username: "happyamy2016", body: newComment})
-    .then((response) => {
-        return response.data.comment;
+  return ncnewsApi
+    .post(`/articles/${article_id}/comments`, {
+      username: "happyamy2016",
+      body: newComment,
     })
-}
+    .then((response) => {
+      return response.data.comment;
+    });
+};
