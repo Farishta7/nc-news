877cf48c35b9a1a1a544f92abd4cabaafc26f703
Farishta Tariq <farishtatq@gmail.com>
2023-10-11 18:13:39 +0100

    seperated articles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
 import Header from "./components/Header";
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import Home from "./components/Home";
 import SingleArticle from './components/SingleArticle';
 import {Route, Routes} from 'react-router-dom';
 import TopicsList from './components/TopicsList';
-import EachTopicArticles from './components/EachTopicArticles';
-
+// import EachTopicArticles from './components/EachTopicArticles';
+import ScrollToTop from './components/ScrollToTop';
+import AllCodingArticles from './components/AllCodingArticles';
+import AllFootballArticles from './components/AllFootballArticles';
+import AllCookingArticles from './components/AllCookingArticles';
+import Weather from './components/Weather';
 
 function App() {  
   return (
     <div className="App">
+      <ScrollToTop />
       <Header />
-      <Nav />
+      {/* <Nav /> */}
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/articles/:article_id' element={<SingleArticle  />}/>
-        <Route path='/topics' element={<TopicsList />}/>
-        <Route path='/topics/:topic' element={<EachTopicArticles />}/>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route path="/topics" element={<TopicsList />} />
+        {/* <Route path="/topics/:topic" element={<EachTopicArticles />} /> */}
+        <Route path="/topics/coding" element={<AllCodingArticles />} />
+        <Route path="/topics/football" element={<AllFootballArticles />} />
+        <Route path="/topics/cooking" element={<AllCookingArticles />} />
+        <Route path="/weather" element={<Weather />} />
       </Routes>
-    
     </div>
   );
 }
