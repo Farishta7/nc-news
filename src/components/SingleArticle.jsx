1ae124515e92834cd4405621f75ed57d49999d1f
Farishta Tariq <farishtatq@gmail.com>
2023-10-11 20:09:50 +0100

    added new icons for voting

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,6 +2,11 @@ import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { getSingleArticle, ArticleUpVote, ArticleDownVote } from "../utils/api";
 import ArticleComments from "../components/ArticleComments";
+import authorIcon from "../images/article-author-icon.png";
+import articleTimeIcon from "../images/article-time-icon.png";
+import thumbsUp from '../images/thumbs-up.png';
+import thumbsDown from "../images/thumbs-down.png";
+
 
 const SingleArticle = () => {
   const [isLoading, setIsloading] = useState(true);
@@ -55,42 +60,62 @@ const SingleArticle = () => {
   if (isLoading) return <p>Hang in there! Loading...</p>;
 
   return (
-    <div className="main-body">
+    <div className="main-body" id="single-article-main-body">
       <div className="body-content">
-      <div className="header-and-image">
-        <h3 className="single-article-header">{singleArticle.title}</h3>
-        <img
-          src={singleArticle.article_img_url}
-          className="single-article-image"
-          alt="I am an alt."
-        />
+        <div className="header-and-image">
+          <h3 className="single-article-header" id="single-article-header">
+            {singleArticle.title}
+          </h3>
+          <img
+            src={singleArticle.article_img_url}
+            className="single-article-image"
+            alt="I am an alt."
+          />
+        </div>
+        <div className="single-article-author">
+          <img src={authorIcon} alt="Author icon" className="author-icon" />
+          {"  "}
+          <div className="author-and-company">
+            <p className="author-name">{singleArticle.author} </p>
+            <p className="author-company-name">
+              Experienced Author{" "}
+              <span style={{ color: "#4a53ba" }}>@CompanyMedia</span>
+            </p>
+          </div>
+        </div>
+        <div className="single-article-time">
+          <img
+            src={articleTimeIcon}
+            alt="Time icon"
+            height={15}
+            className="article-time-icon"
+          />
+          {"  "}
+          <p>{new Date(singleArticle.created_at).toDateString()}</p>
+        </div>
+
+        <div className="voting-section">
+          <img
+            src={thumbsUp}
+            alt="thumbs up icon"
+            className="hithere thumbs"
+            onClick={() => upVote(singleArticle.article_id)}
+          />
+
+          <p className="voting-section">&ensp;{singleArticle.votes}&ensp;</p>
+          <img
+            src={thumbsDown}
+            alt="Thumbs down icon"
+            className="thumbs"
+            onClick={() => downVote(singleArticle.article_id)}
+          />
+          {isVotingError && <p>Your vote was NOT processed!</p>}
+        </div>
+
+        <p className="article-body">{singleArticle.body}</p>
+        <ArticleComments />
       </div>
-      <div style={{ marginTop: "20px" }}>
-        <p className="author-and-date">
-          Author: {singleArticle.author} &ensp; Date posted:{" "}
-          {new Date(singleArticle.created_at).toLocaleString().split(",")[0]}
-        </p>
-      </div>
-      <div>
-        <button
-          className="voting-section"
-          onClick={() => upVote(singleArticle.article_id)}
-        >
-          <span aria-label="up vote for this article">👍</span>
-        </button>
-        <p className="voting-section">&ensp;{singleArticle.votes}&ensp;</p>
-        <button
-          className="voting-section"
-          onClick={() => downVote(singleArticle.article_id)}
-        >
-          <span aria-label="down vote for this article">👎</span>
-        </button>
-        {isVotingError && <p>Your vote was NOT processed!</p>}
-      </div>
-      <p className="article-body">{singleArticle.body}</p>
-      <ArticleComments />
-      </div>
-      </div>
+    </div>
   );
 };
 
