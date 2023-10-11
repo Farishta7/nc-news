a95af3ad577f551dba113f9d32a60333e6c15327
Farishta Tariq <farishtatq@gmail.com>
2023-10-11 18:17:28 +0100

    created new weather page, in production

---
+++ src/components/Weather.jsx
@@ -0,0 +1,13 @@
+const Weather = () => {
+    return (
+      <>
+        <div className="main-body">
+          <div className="body-content">
+            <p className="page-sub-heading">Page coming soon!</p>
+          </div>
+        </div>
+      </>
+    );
+};
+
+export default Weather;
