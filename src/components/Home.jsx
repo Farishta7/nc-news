ffa5a458309c07b9faf09a8f3587116080c026ba
Farishta Tariq <farishtatq@gmail.com>
2023-10-12 10:28:35 +0100

    corrected appearance of loading message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,66 +15,67 @@ const Home = () => {
     });
   }, []);
 
-  if (isLoading)
-    return (
-      <p style={{ fontSize: "30px", paddingLeft: "3rem" }}>
-        Hang in there! Loading...
-      </p>
-    );
+  if (isLoading) {
+    return <p className="page-sub-heading isLoading-text">Hang in there! Loading...</p>;
+  }
 
   return (
-    <div className="main-body">
-      <div className="body-content">
-        <p className="page-sub-heading">ALL ARTICLES</p>
-        <p>{isLoading}</p>
-        <ul className="articles-list">
-          {articles.map((item, index) => {
-            return (
-              <li
-                key={item.article_id}
-                className={`each-article${index === 0 ? " first-article" : ""}`}
-              >
-                <Link to={`/articles/${item.article_id}`}>
-                  <div
-                    className={`grid-container${
-                      index === 0 ? " first-grid-container" : ""
-                    }`}
-                  >
-                    <img
-                      src={item.article_img_url}
-                      className={`images${index === 0 ? " first-image" : ""}`}
-                      alt="I am an alt."
-                      loading="lazy"
-                      id="images-id"
-                    />
-                    <p
-                      className={`article-title${
-                        index === 0 ? " first-article-title" : ""
-                      }`}
-                      id={`article-title${
-                        index === 0 ? " first-article-title" : ""
+    <>
+      <div className="main-body">
+        <div className="body-content">
+          <p className="page-sub-heading">ALL ARTICLES</p>
+          {isLoading}
+          <ul className="articles-list">
+            {articles.map((item, index) => {
+              return (
+                <li
+                  key={item.article_id}
+                  className={`each-article${
+                    index === 0 ? " first-article" : ""
+                  }`}
+                >
+                  <Link to={`/articles/${item.article_id}`}>
+                    <div
+                      className={`grid-container${
+                        index === 0 ? " first-grid-container" : ""
                       }`}
                     >
-                      {item.title}
-                    </p>
-                  </div>
-                </Link>
-                {/* <p>Author: {item.author}</p>
+                      <img
+                        src={item.article_img_url}
+                        className={`images${index === 0 ? " first-image" : ""}`}
+                        alt="I am an alt."
+                        loading="lazy"
+                        id="images-id"
+                      />
+                      <p
+                        className={`article-title${
+                          index === 0 ? " first-article-title" : ""
+                        }`}
+                        id={`article-title${
+                          index === 0 ? " first-article-title" : ""
+                        }`}
+                      >
+                        {item.title}
+                      </p>
+                    </div>
+                  </Link>
+                  {/* <p>Author: {item.author}</p>
                 <p>
                   {new Date(item.created_at).toLocaleString().split(",")[0]}
                 </p>
                 <p>
                   {item.topic.charAt(0).toUpperCase() + item.topic.slice(1)}
                 </p> */}
-              </li>
-            );
-          })}
-          {/* <li className="each-article empty-article-dummy">
+                </li>
+              );
+            })}
+            {/* <li className="each-article empty-article-dummy">
             
           </li> */}
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
